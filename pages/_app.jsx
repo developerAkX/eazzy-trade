844f713b7b36c47c0c2ccab7bfce45c5e54758f8
Ayushks11560 <54079897+Ayushks11560@users.noreply.github.com>
2022-01-08 15:23:06 +0530

    Added: tsParticales

---
+++ pages/_app.jsx
@@ -0,0 +1,176 @@
+import Header from "../components/AppBar/Header";
+import Faas from "../components/Footer/faas";
+import styled from "@emotion/styled";
+import "../styles/globals.css";
+import LoadingScreen from "../components/LoadingScreen";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import Particles from "react-tsparticles";
+
+function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, [router]);
+  return (
+    <Root>
+      <Particles
+        id="tsparticles"
+        options={{
+          fullScreen: {
+            enable: true,
+            zIndex: 1,
+          },
+          detectRetina: true,
+          fpsLimit: 60,
+          interactivity: {
+            events: {
+              onClick: {
+                enable: true,
+                mode: "push",
+              },
+              onDiv: {
+                elementId: "repulse-div",
+                enable: true,
+                mode: "repulse",
+              },
+              onHover: {
+                enable: true,
+                mode: "bubble",
+                parallax: {
+                  enable: false,
+                  force: 60,
+                  smooth: 10,
+                },
+              },
+              resize: true,
+            },
+            modes: {
+              bubble: {
+                distance: 400,
+                duration: 2,
+                opacity: 0.8,
+                size: 2,
+              },
+              connect: {
+                distance: 80,
+                lineLinked: {
+                  opacity: 0.5,
+                },
+                radius: 60,
+              },
+              grab: {
+                distance: 400,
+                lineLinked: {
+                  opacity: 1,
+                },
+              },
+              push: {
+                quantity: 2,
+              },
+              remove: {
+                quantity: 2,
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.4,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: "#1976d2",
+            },
+            lineLinked: {
+              blink: false,
+              color: "#000",
+              consent: false,
+              distance: 150,
+              enable: false,
+              opacity: 0.4,
+              width: 1,
+            },
+            move: {
+              attract: {
+                enable: false,
+                rotate: {
+                  x: 600,
+                  y: 1200,
+                },
+              },
+              bounce: false,
+              direction: "none",
+              enable: true,
+              outMode: "out",
+              random: false,
+              speed: 2,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                area: 800,
+              },
+              limit: 20,
+              value: 15,
+            },
+            opacity: {
+              animation: {
+                enable: true,
+                minimumValue: 0.2,
+                speed: 1,
+                sync: false,
+              },
+              random: true,
+              value: 1,
+            },
+            rotate: {
+              animation: {
+                enable: true,
+                speed: 5,
+                sync: false,
+              },
+              direction: "random",
+              random: true,
+              value: 0,
+            },
+
+            size: {
+              animation: {
+                enable: false,
+                minimumValue: 0.1,
+                speed: 40,
+                sync: false,
+              },
+              random: false,
+              value: 16,
+            },
+          },
+        }}
+      />
+      {!loading ? (
+        <>
+          <Header />
+          <Component {...pageProps} />
+          <Faas />
+        </>
+      ) : (
+        <LoadingScreen />
+      )}
+    </Root>
+  );
+}
+
+export default MyApp;
+
+const Root = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  height: 100vh;
+`;
